6a65d6c67cc2fa532e648476442d54f1fb23ecd9
TheAlpha-0218 <159509423+TheAlpha-0218@users.noreply.github.com>
2025-03-09 17:12:57 +0530

    feat: Implement authentication controller and connect to MongoDB

--- Chat-backend/src/routes/auth.route.js
+++ Chat-backend/src/routes/auth.route.js
@@ -1,18 +1,10 @@
 import express from 'express';
+import { signup, login, logout } from '../controllers/auth.controller.js';
 
 const router=express.Router();
 
-router.get("/signup", (req,res)=>{
-    res.send("signup Route");
-});
-
-router.get("/login", (req,res)=>{
-    res.send("login Route");
-});
-
-router.get("/logout", (req,res)=>{
-    res.send("logout Route");
-});
-
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/logout", logout);
 
 export default router;
