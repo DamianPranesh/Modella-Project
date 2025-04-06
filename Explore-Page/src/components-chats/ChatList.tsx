accd834e3c7fdccca00031dc6761f16ad7827336
LagshanR <lagshan03@gmail.com>
2025-04-06 21:06:18 +0530

    Minor change in chat count

--- Explore-Page/src/components-chats/ChatList.tsx
+++ Explore-Page/src/components-chats/ChatList.tsx
@@ -44,7 +44,7 @@ const Chatlist: React.FC = () => {
             />
             <span className="text-sm">Show online only</span>
           </label>
-          <span className="text-xs text-zinc-500">({onlineUsers.length - 1} online)</span>
+          <span className="text-xs text-zinc-500">({onlineUsers.length} online)</span>
         </div>
       </div>
 
